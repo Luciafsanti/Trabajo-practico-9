91a5a4b930dc3684028753c58818c3781e4f26c4
Luciafsanti <luciafsanti@gmail.com>
2024-04-15 19:26:27 -0300

    navbar columna

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
@@ -9,6 +9,12 @@ const NavbarContainer = styled.nav`
   background-color: var(--Reseda-green);
   padding: 1rem 2rem;
   flex-wrap: wrap;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
 `;
 
 const NavLink = styled(Link)`
@@ -21,6 +27,7 @@ const NavLink = styled(Link)`
   &:hover {
     background-color: var(--Reseda-green);
   }
+
   @media (min-width: 768px){
     padding: 1rem 0.5rem;
     font-size: 1rem;
@@ -32,44 +39,51 @@ const Logo = styled.div`
   font-family: var(--logofont);
   font-size: 20px;
   font-weight: lighter;
+  text-decoration: none;
 
-  @media (min-width: 768px){
+  @media (min-width: 768px) {
     font-size: 30px;
     font-weight: normal;
-}
+  }
+
+  && a{
+    color: var(--WhiteSmoke);
+    text-decoration: none;
+    display: flex;
+    align-content: center;
+    gap: 0.5em
+  }
 `;
 
 const LogoImg = styled.img`
   width: 2.5rem;
   height: 2.5rem;
-  border-radius:100%;
-`
+  border-radius: 100%;
+`;
 
 const SearchContainer = styled.div`
   display: flex;
   align-items: center;
+  margin-bottom: 1rem;
 
-  @media (min-width: 768px){
-    
-}
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+  }
 `;
 
 const SearchInput = styled.input`
   border-radius: 4px;
   border: 1px solid #ccc;
-  hidden: true;
-
-  @media (min-width: 768px){
-    padding: 0.5rem;
-    margin-right: 0.5rem;
-  }
+  padding: 0.5rem;
+  margin-right: 0.5rem;
 `;
 
-const SearchButton = styled.button`
+const Button = styled.button`
   color: var(--WhiteSmoke);
-  background-color: var(--Reseda-green), 75%;
+  background-color: var(--Reseda-green);
   padding: 0.5rem;
   border: none;
+  border-radius: 4px;
   cursor: pointer;
   transition: background-color 0.3s ease;
 
@@ -78,40 +92,70 @@ const SearchButton = styled.button`
   }
 `;
 
-const FilterButton = styled.button`
-  color: var(--WhiteSmoke);
-  background-color: var(--Reseda-green), 75%;
-  padding: 0.5rem;
-  border: none;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+const SearchButton = styled(Button)`
+  margin-right: 0.5rem;
+`;
+
+const FilterButton = styled(Button)`
   margin-right: 1rem;
+`;
 
-  &:hover {
-    background-color: var(--Reseda-green);
+const MenuIcon = styled.button`
+  display: none;
+  background: none;
+  border: none;
+  color: var(--WhiteSmoke);
+  font-size: 1.5rem;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
+
+
+
+const NavbarMenu = styled.div`
+  display: ${(props) => (props.isOpen ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
   }
 `;
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <NavbarContainer>
       <Logo>
         <Link to="/">
           <LogoImg src="/LogoImg.jfif" alt="Logo" />
-        </Link>
-        Aromito Librería
+          Aromito Librería </Link>
       </Logo>
       <SearchContainer>
         <SearchInput type="text" placeholder="Buscar..." />
         <SearchButton>Buscar</SearchButton>
         <FilterButton>Filtro</FilterButton>
       </SearchContainer>
-      <div>
+      <MenuIcon onClick={toggleMenu}>
+        &#9776;
+      </MenuIcon>
+      <NavbarMenu isOpen={isOpen}>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/login">Iniciar sesión</NavLink>
         <NavLink to="/register">Registrarse</NavLink>
         <NavLink to="/cart">Carrito</NavLink>
-      </div>
+      </NavbarMenu>
     </NavbarContainer>
   );
 };
